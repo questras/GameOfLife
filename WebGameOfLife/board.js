77d60bf5875f1599f1c140a0d1ba8daeaf3ac24a
questras <michu123qwe@gmail.com>
2020-06-17 10:32:13 +0200

    change size and look of board, add speed slider and refactor code

--- WebGameOfLife/board.js
+++ WebGameOfLife/board.js
@@ -1,17 +1,22 @@
-var WIDTH = 40;
-var HEIGHT = 20;
+var WIDTH = 120;
+var HEIGHT = 58;
 var THRESHOLD = 500;
 var INTERVAL = null;
+
+var ALIVE_COLOR = "white";
+var DEAD_COLOR = "black";
 
 // Create board of boxes with WIDTH x HEIGHT size.
 function createBoard() {
     var boxDiv = document.getElementById("boxes");
     for (var row = 0; row < HEIGHT; row++) {
+        var boxRow = document.createElement("div");
+        boxRow.className = "row";
         for (var col = 0; col < WIDTH; col++) {
             var box = createBox(row, col);
-            boxDiv.appendChild(box);
+            boxRow.appendChild(box);
         }
-        boxDiv.appendChild(document.createElement("br"));
+        boxDiv.appendChild(boxRow);
     }
 }
 
@@ -20,7 +25,7 @@ function createBox(row, col) {
     var box = document.createElement("button");
     box.innerHTML = "";
     box.className = "box";
-    box.style.backgroundColor = "red";
+    box.style.backgroundColor = DEAD_COLOR;
     box.id = "box" + row + "-" + col;
     box.onclick = () => (changeColor(row, col));
 
@@ -50,30 +55,30 @@ function getColor(row, col) {
 }
 
 // Return value of box in (row, col) i.e
-// 1 if box is green (alive), 0 otherwise.
+// 1 if box is alive, 0 otherwise.
 function getValue(row, col) {
     if (row < 0 || col < 0 || row >= HEIGHT || col >= WIDTH) {
         return 0;
     }
 
     var box = getBox(row, col);
-    return box.style.backgroundColor == "green" ? 1 : 0;
+    return box.style.backgroundColor == ALIVE_COLOR ? 1 : 0;
 }
 
 // Change color of box in (row, col) to opposite.
 function changeColor(row, col) {
-    if (getColor(row, col) == "green") {
-        setColor(row, col, "red");
+    if (getColor(row, col) == ALIVE_COLOR) {
+        setColor(row, col, DEAD_COLOR);
     } else {
-        setColor(row, col, "green");
+        setColor(row, col, ALIVE_COLOR);
     }
 }
 
-// Set all color of all boxes on board to red (not alive).
+// Set all color of all boxes on board to dead.
 function resetColors() {
     for (var row = 0; row < HEIGHT; row++) {
         for (var col = 0; col < WIDTH; col++) {
-            setColor(row, col, "red");
+            setColor(row, col, DEAD_COLOR);
         }
     }
 }
@@ -98,6 +103,18 @@ function isAliveNextTurn(row, col) {
     }
 }
 
+// Increment number of turns.
+function incrementTurn() {
+    var turn = document.getElementById("turn");
+    turn.innerHTML = parseInt(turn.innerHTML) + 1;
+}
+
+// Change number of turns to 0.
+function resetTurn() {
+    var turn = document.getElementById("turn");
+    turn.innerHTML = 0;
+}
+
 // Simulate one turn of game of life.
 function simulateTurn() {
     var board = new Array(HEIGHT);
@@ -114,23 +131,36 @@ function simulateTurn() {
     for (var row = 0; row < HEIGHT; row++) {
         for (var col = 0; col < WIDTH; col++) {
             if (board[row][col]) {
-                setColor(row, col, "green");
+                setColor(row, col, ALIVE_COLOR);
             } else {
-                setColor(row, col, "red");
+                setColor(row, col, DEAD_COLOR);
             }
             
         }
     }
+    incrementTurn();
 }
 
 // Simulate one turn of Game of Life every given THRESHOLD time, run
 // until stopped by stopSimulation.
 function simulate() {
-    INTERVAL = setInterval(simulateTurn, THRESHOLD);
+    var threshold = document.getElementById("threshold");
+    thresholdValue = threshold.value * 100;
+    threshold.disabled = true;
+    INTERVAL = setInterval(simulateTurn, thresholdValue);
 }
 
 // Stop already running simulation.
 function stopSimulation() {
     clearInterval(INTERVAL);
     INTERVAL = null;
+    var threshold = document.getElementById("threshold");
+    threshold.disabled = false;
+}
+
+// Restart game i.e clear board and reset turns.
+function reset() {
+    resetColors();
+    resetTurn();
+    stopSimulation();
 }
